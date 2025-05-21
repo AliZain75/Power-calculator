2088ede1f6cbf6e1f41d3e2cdefec0165bc89ffc
AliZain <alizain.vibes@gmail.com>
2025-05-21 17:23:19 +1000

    Mirrored  Triangles

--- Reverse Order.py
+++
@@ -1,6 +0,0 @@
-def count_digits(number):
-    number_str = str(abs(number)) 
-    return len(number_str)
-num = int(input("Enter a number: "))
-digit_count = count_digits(num)
-print(f"The number {num} has {digit_count} digit(s).")
