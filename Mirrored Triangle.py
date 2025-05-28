010af318e3984e968fe9da28411327c91da09f9e
AliZain <alizain.vibes@gmail.com>
2025-05-28 17:14:54 +1000

    Square

--- Mirrored Triangle.py
+++
@@ -1,7 +0,0 @@
-rows = int(input("Enter the number of rows: "))
-
-print("\nRight-Angled Triangle and Mirrored Triangle:\n")
-for i in range(1, rows + 1):
-    left_triangle = '*' * i
-    right_triangle = ' ' * (rows - i) + '*' * i
-    print(left_triangle + '   ' + right_triangle)
